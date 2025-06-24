003ab198169aa96001d58a67029c55464b16f329
User <u.web.dev@gmail.com>
2025-06-25 00:11:19 +0300

    fix env to CUSTOM_BACK_URL on communication file

--- app/util/communication.ts
+++ app/util/communication.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL =
-    process.env.WORDPRESS_API_URL || "http://leadr.local/wp-json/wp/v2";
+    process.env.CUSTOM_BACK_URL || "http://leadr.local/wp-json/wp/v2";
 
 export const wpApi = axios.create({
     baseURL: API_URL,
